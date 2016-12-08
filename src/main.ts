55871c05fa6832df840bccbe8f7c2419ca0b0102
Brent Hill <brentc4m@users.noreply.github.com>
2016-12-07 22:47:22 -0800

    Bootstrapped electron and typescript

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import electron = require("electron")
+import path = require("path")
+import url = require("url")
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "../index.html"),
+    protocol: "file:",
+    slashes: true
+  }))
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on("closed", () => {
+    mainWindow = null
+  })
+}
+
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit()
+  }
+})
+
+app.on("activate", () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
